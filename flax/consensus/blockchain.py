58351afec41f78e031507d98b000db2087c2c13f
grayfallstown <grayfallstown@protonmail.com>
2021-08-18 19:07:29 +0200

    added a config option to reduce the number of full_node worker processes to reduce ram usage

--- flax/consensus/blockchain.py
+++ flax/consensus/blockchain.py
@@ -35,6 +35,8 @@ from flax.util.errors import Err
 from flax.util.generator_tools import get_block_header, tx_removals_and_additions
 from flax.util.ints import uint16, uint32, uint64, uint128
 from flax.util.streamable import recurse_jsonify
+from flax.util.default_root import DEFAULT_ROOT_PATH
+from flax.util.config import load_config
 
 log = logging.getLogger(__name__)
 
@@ -101,7 +103,10 @@ class Blockchain(BlockchainInterface):
         cpu_count = multiprocessing.cpu_count()
         if cpu_count > 61:
             cpu_count = 61  # Windows Server 2016 has an issue https://bugs.python.org/issue26903
+        config = load_config(DEFAULT_ROOT_PATH, "config.yaml")
         num_workers = max(cpu_count - 2, 1)
+        if 'multiprocessing_limit' in config.keys():
+            num_workers = min(num_workers, int(config["multiprocessing_limit"]));
         self.pool = ProcessPoolExecutor(max_workers=num_workers)
         log.info(f"Started {num_workers} processes for block validation")
 
